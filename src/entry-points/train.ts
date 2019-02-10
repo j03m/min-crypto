9069b43ea956b79899292020705c47c65b1acd14
Joe Mordetsky <jmordetsky@zynga.com>
2019-02-10 07:29:58 -0500

    Model trains, need to switch ts for ts node and look at bindings

--- src/entry-points/train.ts
+++ src/entry-points/train.ts
@@ -1,6 +1,64 @@
-//https://js.tensorflow.org/api/0.14.2/#data.csv
-import * as tf from "@tensorflow/tfjs-node";
-const csvUrl = "file://../../ai/training-feb-1-90d/data.csv";
+// //https://js.tensorflow.org/api/0.14.2/#data.csv
+// import * as tf from "@tensorflow/tfjs";
+// import {CSVDataset} from "@tensorflow/tfjs-data";
+// const csvUrl = "file://./ai/training-feb-1-90d/data.csv";
+// async function run() {
+//     // We want to predict the column "medv", which represents a median value of
+//     // a home (in $1000s), so we mark it as a label.
+//     const csvDataset:CSVDataset = tf.data.csv(
+//         csvUrl, {
+//             columnConfigs: {
+//                 action: {
+//                     isLabel: true
+//                 }
+//             }
+//         });
+//
+//     // Number of features is the number of column names minus one for the label
+//     // column.
+//     const numOfFeatures = (await csvDataset.columnNames()).length - 1;
+//
+//     // Prepare the Dataset for training.
+//     const flattenedDataset =
+//         csvDataset
+//             .map(([rawFeatures, rawLabel]) =>
+//                 // Convert rows from object form (keyed by column name) to array form.
+//                 [Object.values(rawFeatures), Object.values(rawLabel)])
+//             .batch(10);
+//
+//     // Define the model.
+//     const model = tf.sequential();
+//     model.add(tf.layers.dense({
+//         inputShape: [numOfFeatures],
+//         units: 10
+//     }));
+//     model.compile({
+//         optimizer: tf.train.sgd(0.000001),
+//         loss: 'meanSquaredError'
+//     });
+//
+//     // Fit the model using the prepared Dataset
+//     return model.fitDataset(flattenedDataset, {
+//         epochs: 10,
+//         callbacks: {
+//             onEpochEnd: async (epoch, logs) => {
+//                 console.log(epoch + ':' + logs.loss);
+//             }
+//         }
+//     });
+// }
+//
+// run().then((model)=>{
+//     console.log("done!", model);
+//     return model.save('file://../../ai/training-feb-1-90d/');
+// });
+//
+// //strategy would: const model = await tf.loadModel('file:///tmp/my-model-1/model.json');
+
+import * as tf from "@tensorflow/tfjs";
+//const csvUrl = "file://./ai/training-feb-1-90d/boston-housing-train.csv";
+const csvUrl = "file://./ai/training-feb-1-90d/data.csv";
+
 async function run() {
     // We want to predict the column "medv", which represents a median value of
     // a home (in $1000s), so we mark it as a label.
@@ -10,19 +68,23 @@ async function run() {
                 action: {
                     isLabel: true
                 }
-            }
+            },
+            hasHeader:true
         });
 
     // Number of features is the number of column names minus one for the label
     // column.
-    const numOfFeatures = (await csvDataset.columnNames()).length - 1;
+    const numOfFeatures = (await csvDataset.columnNames()).length - 2;
 
     // Prepare the Dataset for training.
     const flattenedDataset =
         csvDataset
-            .map(([rawFeatures, rawLabel]) =>
+            .map(([rawFeatures, rawLabel]) => {
                 // Convert rows from object form (keyed by column name) to array form.
-                [Object.values(rawFeatures), Object.values(rawLabel)])
+                const trimDate = Object.values(rawFeatures);
+                const final = trimDate.slice(1, trimDate.length);
+                return [final, Object.values(rawLabel)]
+            })
             .batch(10);
 
     // Define the model.
@@ -47,9 +109,6 @@ async function run() {
     });
 }
 
-run().then((model)=>{
-    console.log("done!", model);
-    return model.save('file://../../ai/training-feb-1-90d/');
+run().then((out)=>{
+    console.log(out);
 });
-
-//strategy would: const model = await tf.loadModel('file:///tmp/my-model-1/model.json');
