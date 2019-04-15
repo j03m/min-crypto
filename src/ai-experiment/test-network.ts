7db18d6ae2066969a6ac5ebca27e908ec40af527
Joe Mordetsky <jmordetsky@zynga.com>
2019-04-15 17:04:21 -0400

    saving

--- src/ai-experiment/test-network.ts
+++ src/ai-experiment/test-network.ts
@@ -1,5 +1,10 @@
-import Network from "./network";
+// import Network from "./network";
+//
+// const network = new Network();
+// network.generate();
+// console.log("I think: ", network.compute([-2,-1]));
 
-const network = new Network();
-network.generate();
-console.log("I think: ", network.compute([-2,-1]));
+
+const mth = require("mathjs");
+
+console.log(mth.derivative('f(x) = x^2 + x', 'x'))
